fbfd88af470561d717748331f9fc3e77d0f6c9e8
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2023-02-28 00:16:21 +0300

    Start to Queue

---
+++ src/components/queue-page/initQueue.ts
@@ -0,0 +1,67 @@
+interface IQueue<T> {
+    enqueue: (item: T) => {
+        head: number,
+        tail: number,
+        body: Array<T | null>
+    };
+    dequeue: () => void;
+    resqueue: () => void;
+    peak: () => T | null;
+  }
+  
+  export class Queue<T> implements IQueue<T> {
+    private container: (T | null)[] = [];
+    private head = 0;
+    private tail = 0;
+    private readonly size: number = 0;
+    private length: number = 0;
+  
+    constructor(size: number) {
+      this.size = size;
+      this.container = Array(size);
+    }
+  
+    enqueue = (item: T) => {
+      if (this.length >= this.size) {
+        throw new Error("Maximum length exceeded");
+      }
+      this.container[this.tail % this.size] = item
+      this.tail = this.tail+1;
+      this.length = this.length+1;
+      return {
+        head: this.head,
+        tail: this.tail,
+        body: this.container
+     }
+    };
+  
+    dequeue = () => {
+      if (this.isEmpty()) {
+        throw new Error("No elements in the queue");
+      }
+      
+      if (this.length === 1) {
+        this.head = 0;
+        this.tail = 0;
+        this.length = 0;
+      } else {
+        this.head = this.head +1;
+        this.length = this.length -1;
+      }
+    };
+    resqueue = () => {
+      this.head = 0;
+      this.tail = 0;
+      this.length = 0
+    }
+  
+    peak = (): T | null => {
+      if (this.isEmpty()) {
+        throw new Error("No elements in the queue");
+      }
+      //доступ к последнему элементу в очереди
+      return this.container[this.head % this.size] || null; 
+    };
+  
+    isEmpty = () => this.length === 0;
+  }
--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,9 +1,92 @@
-import React from "react";
+import React, { FormEvent, useEffect, useState } from "react";
+import { ElementStates } from "../../types/element-states";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { HEAD, TAIL } from "../../constants/element-captions";
+import { Queue } from "./initQueue";
+import style from './queue-page.module.css';
+
+
+const queue = new Queue<string>(6);
 
 export const QueuePage: React.FC = () => {
+  const [items, setItems] = useState<Array<string | null>>(Array('', '', '', '', '', ''))
+  const [head, setHead] = useState(0);
+  const [tail, setTail] = useState(0)
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const input = (e.target as HTMLFormElement).elements[1]
+    const data = (input as HTMLInputElement).value
+    addEl(data);
+    (input as HTMLInputElement).value = ''
+  }
+
+  const addEl = (data: string) => {
+    let {head, tail, body} = queue.enqueue(data);
+    setHead(head);
+    setTail(tail);
+    const arr = items;
+    for (let i = head; i< tail; i++) {
+      arr[i] = body[i];
+    }
+    setItems([...arr])
+  } 
+
+  const onDelete = () => {
+    queue.dequeue();
+    const arr = items;
+    arr[head] = '';
+    setHead(head+1);
+    setItems([...arr])
+  }
+  const setDescriptions = (item: string | null, index: number) => {
+    const descriptions: Array<string | null>= [null, null]
+    if(item && index === head) { descriptions[0] = HEAD }
+    if(item && index === tail-1) { descriptions[1] = TAIL }
+    return descriptions
+  }
+
+  const onReset = () => {
+    queue.resqueue();
+    setItems(Array('', '', '', '', '', ''))
+  }
+  
+ 
   return (
     <SolutionLayout title="Очередь">
+      <form className={style.form} onSubmit={onSubmit} onReset={onReset}>
+        <fieldset className={style.container} >
+          <Input 
+          maxLength={4} 
+          isLimitText={true}
+          id='input' />
+          <Button
+            text='Добавить'
+            type='submit' />
+          <Button
+            text="Удалить"
+            type='button'
+            onClick={onDelete} />
+        </fieldset>
+        <Button
+          text="Очистить"
+          type='reset' />
+      </form>
+      <ul className={style.display}>
+          {items && items.map((item, index) =>
+            <li className={style.circle} key={index}>
+              <Circle
+                head={setDescriptions(item, index)[0]}
+                tail={setDescriptions(item, index)[1]}
+                letter={`${item}`}
+                index={index}
+              />
+            </li>
+          )}
+        </ul>
 
     </SolutionLayout>
   );
