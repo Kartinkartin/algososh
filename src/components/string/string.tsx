16ace3500f15ce30c3964ffdc52787eb4ae3b345
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2023-03-02 22:30:04 +0300

    Add coloring, button props: loader, disabled

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -9,73 +9,88 @@ import { ElementStates } from "../../types/element-states";
 
 export const StringComponent: React.FC = () => {
   const [loader, setLoader] = useState(false);
+  const [isDisabled, setDisabled] = useState(true);
   const [letters, setLetters] = useState<Array<string> | null>(null)
   const [counter, setCounter] = useState<number>(-1);
+  const input = (document.forms.namedItem('reverseForm')?.elements[0] as HTMLInputElement);
+
+
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // setLoader(true)
+    setLoader(true);
     setCounter(-1);
-    const input = document.querySelector('input')!;
-    const str = input.value;
+    const str = input?.value;
     reverseFunc(str);
     input.value = '';
+  }
+
+  const onChange = () => {
+    if(input.value === '') {
+      setDisabled(true)
+    } else {
+      setDisabled(false)
+    }
   }
 
   const reverseFunc = (str: string) => {
     const arr = str.split('');
     setLetters(arr);
-    // setTimeout(`${DELAY_IN_MS}`);
-    // for(let i =0; i < arr.length/2; i++ ){
-    //   setTimeout(setLetters, DELAY_IN_MS,(letters: any)=>{ 
-    //     const arr = letters.slice();
-    //     debugger
-    //     swap(arr, i); 
-    //     setCounter(i+1);
-    //     return [...arr] 
-    //   })
-    // }
   }
   //меняет местами два элемента 
   const swap = (arr: Array<string>, index: number) => {
     const tmp = arr[index];
     arr[index] = arr[arr.length - 1 - index];
-    arr[arr.length - 1 - index] = tmp
+    arr[arr.length - 1 - index] = tmp;
   }
+  // устанавливает цвет элемента
   const typeHandler = (arr: Array<string>, index: number, counter: number) => {
-    if (index < counter || index > arr.length - 1 - counter) { return ElementStates.Changing }
-    if (index > counter && index < arr.length - 1 - counter) { return ElementStates.Default }
-    if (index === counter || index === arr.length - 1 - counter) { return ElementStates.Modified }
+    if (index < counter || index > arr.length - 1 - counter) { 
+      return ElementStates.Changing 
+    }
+    if (index > counter && index < arr.length - 1 - counter) { 
+      return ElementStates.Default 
+    }
+    if (index === counter || index === arr.length - 1 - counter) { 
+      return ElementStates.Modified 
+    }
   }
   useEffect(() => {
     if (counter === -1) {
-      setTimeout(setCounter, DELAY_IN_MS, 0)
+      setTimeout(setCounter, DELAY_IN_MS, 0);
     } else {
-      const index = counter
-      const delay = DELAY_IN_MS * (index + 1);
-      if (letters?.length && index < letters.length / 2) {
-        setTimeout(setLetters, delay, (letters: any) => {
-          const arr = letters.slice();
-          debugger
+      const index = counter;
+      if (letters?.length && index < letters.length/2) {
+        setTimeout(setLetters, DELAY_IN_MS, (letters: any) => {
+          const arr = [...letters];
           swap(arr, index);
-          return [...arr]
+          setCounter(index + 1);
+          return [...arr];
         })
-        setCounter(index + 1);
       }
+    }
+    if(letters?.length && counter >= letters?.length/2) {
+      setLoader(false);
     }
   }, [letters, counter])
 
   return (
     <SolutionLayout title="Строка">
       <div className={style.main}>
-        <form className={style.form} onSubmit={(e) => { onSubmit(e) }}>
+        <form
+          className={style.form}
+          name='reverseForm'
+          onSubmit={(e) => { onSubmit(e) }}>
           <Input
             maxLength={11}
             isLimitText={true}
+            onChange={onChange}
           />
           <Button
             text='Развернуть'
             type='submit'
             isLoader={loader}
+            extraClass='ml-6'
+            disabled={isDisabled}
           />
         </form>
         <ul className={style.container}>
@@ -84,6 +99,7 @@ export const StringComponent: React.FC = () => {
               state={typeHandler(letters, index, counter)}
               key={index}
               letter={letter}
+              extraClass='ml-8 mr-8'
             />
           )}
         </ul>
