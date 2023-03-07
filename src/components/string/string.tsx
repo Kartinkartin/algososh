737714de5a769cea99f97b66c07a96538e0f9edf
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2023-03-07 15:17:51 +0300

    Fix: Add useForm hook, include useForm to String

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import style from './string.module.css';
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
@@ -6,35 +6,29 @@ import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { DELAY_IN_MS } from "../../constants/delays";
 import { ElementStates } from "../../types/element-states";
+import { useForm } from "../../hooks/useForm";
 
 export const StringComponent: React.FC = () => {
   const [loader, setLoader] = useState<boolean>(false);
-  const [isDisabled, setDisabled] = useState<boolean>(true);
   const [letters, setLetters] = useState<Array<string> | null>(null)
   const [counter, setCounter] = useState<number>(-1);
-  const input = (document.forms.namedItem('reverseForm')?.elements[0] as HTMLInputElement);
+  const name = 'stringInput';
+  const { values, handleChange, setValues } = useForm({ [name]: '' });
 
 
-  const onSubmit = (e: React.FormEvent) => {
+  const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     setLoader(true);
     setCounter(-1);
-    const str = input?.value;
+    const str = values[name];
     reverseFunc(str);
-    input.value = '';
-  }
-
-  const onChange = () => {
-    if(input.value === '') {
-      setDisabled(true)
-    } else {
-      setDisabled(false)
-    }
+    setValues({...values, [name]: ''})
   }
 
   const reverseFunc = (str: string) => {
     const arr = str.split('');
     setLetters(arr);
+
   }
 
   //меняет местами два элемента 
@@ -57,13 +51,17 @@ export const StringComponent: React.FC = () => {
     }
   }
 
+  useEffect(()=>{
+    setValues({ [name]: ''})
+  }, [setValues])
+
   useEffect(() => {
     if (counter === -1) {
       setTimeout(setCounter, DELAY_IN_MS, 0);
     } else {
       const index = counter;
       if (letters?.length && index < letters.length/2) {
-        setTimeout(setLetters, DELAY_IN_MS, (letters: any) => {
+        setTimeout(setLetters, DELAY_IN_MS, (letters: Array<string>) => {
           const arr = [...letters];
           swap(arr, index);
           setCounter(index + 1);
@@ -82,18 +80,20 @@ export const StringComponent: React.FC = () => {
         <form
           className={style.form}
           name='reverseForm'
-          onSubmit={(e) => { onSubmit(e) }}>
+          onSubmit={onSubmit}>
           <Input
             maxLength={11}
             isLimitText={true}
-            onChange={onChange}
+            value={values[name]}
+            onChange={handleChange}
+            name={name}
           />
           <Button
             text='Развернуть'
             type='submit'
             isLoader={loader}
             extraClass='ml-6'
-            disabled={isDisabled}
+            disabled={!values[name]}
           />
         </form>
         <ul className={style.container}>
