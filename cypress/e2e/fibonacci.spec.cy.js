f02ec41967a8e8ec61245800d32c7766b6c81b0d
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2023-03-22 21:22:44 +0300

    Made e2e test fibonacci component

--- cypress/e2e/fibonacci.spec.cy.js
+++ cypress/e2e/fibonacci.spec.cy.js
@@ -1,5 +1,7 @@
+import { SHORT_DELAY_IN_MS } from "../../src/constants/delays";
+
 describe('testing Fibonacci component', () => {
-  before(()=>{
+  beforeEach(()=>{
     cy.visit('/fibonacci');
   })
 
@@ -12,5 +14,35 @@ describe('testing Fibonacci component', () => {
     cy.get('@button').should('not.be.disabled');
     cy.get('@input').clear();
     cy.get('@button').should('be.disabled');
+  })
+
+  it('correct generation numbers', () => {
+    cy.get('input').type('3');
+    cy.get('button[type=submit]').as('button')
+    cy.get('@button').click();
+    cy.clock(new Date(), ['Date']);
+    cy.wait(SHORT_DELAY_IN_MS);
+
+    // cy.get('ul>li>div').should('have.length', '1').then((els) => {
+    //   cy.get(els[0]).get('div[class*="circle_circle"]>p').should('have.text', '1');
+    // })
+
+    cy.wait(SHORT_DELAY_IN_MS);
+
+    cy.get('ul>li>div').should('have.length', '2').then((els) => {
+      cy.get(els).get('div[class*="circle_circle"]>p').should('have.text', '11')
+    })
+
+    cy.wait(SHORT_DELAY_IN_MS);
+
+    cy.get('ul>li>div').should('have.length', '3').then((els) => {
+      cy.get(els).get('div[class*="circle_circle"]>p').should('have.text', '112')
+    })
+
+    cy.wait(SHORT_DELAY_IN_MS);
+
+    cy.get('ul>li>div').should('have.length', '4').then((els) => {
+      cy.get(els).get('div[class*="circle_circle"]>p').should('have.text', '1123')
+    })
   })
 })
